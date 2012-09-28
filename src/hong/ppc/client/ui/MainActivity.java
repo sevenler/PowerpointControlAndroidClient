daa75e6371ce8921984617ccab6d83a9579c1382
songwei <johnnyxyz@163.com>
2012-09-28 17:13:53 +0800

    添加IP输入检测

--- src/hong/ppc/client/ui/MainActivity.java
+++ src/hong/ppc/client/ui/MainActivity.java
@@ -1,6 +1,10 @@
 package hong.ppc.client.ui;
 
 import hong.ppc.client.R;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +12,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -26,12 +31,27 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				String ip = ipaddress.getText().toString();
-				Intent intent = new Intent(MainActivity.this,
-						ControlActivity.class);
-				intent.putExtra("ip", ip);
-				startActivity(intent);
+				if (MainActivity.isboolIp(ip)) {
+					Intent intent = new Intent(MainActivity.this,
+							ControlActivity.class);
+					intent.putExtra("ip", ip);
+					startActivity(intent);
+				} else {
+					Toast.makeText(MainActivity.this,
+							R.string.alert_not_ip_address, Toast.LENGTH_SHORT)
+							.show();
+				}
 			}
 		});
+	}
 
+	public static boolean isboolIp(String ipAddress) {
+		String ip = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+				+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+				+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\."
+				+ "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+		Pattern pattern = Pattern.compile(ip);
+		Matcher matcher = pattern.matcher(ipAddress);
+		return matcher.matches();
 	}
 }
